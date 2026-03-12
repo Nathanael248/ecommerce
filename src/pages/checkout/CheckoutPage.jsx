0dff1340b762cef4578a589bc8e2d19fcce1cfc7
Nathanael248 <okunadenathaniel@gmail.com>
2026-03-12 18:50:13 +0000

    Separate into smaller components.

---
+++ src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { OrderSummary } from "./OrderSummary";
+import { PaymentSummary } from "./PaymentSummary";
+import { useEffect, useState } from "react";
+import "./CheckoutPage.css";
+
+import { Header } from "../../components/Header";
+// import "./checkout-header.css";
+
+// product images are served from public folder
+
+export function CheckoutPage({ cart }) {
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+  const [paymentSummary, setPaymentSummary] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/delivery-options?expand=estimatedDeliveryTime")
+      .then((response) => {
+        setDeliveryOptions(response.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get("/api/payment-summary").then((response) => {
+      setPaymentSummary(response.data);
+    });
+  }, []);
+  return (
+    <>
+      <title>Checkout</title>
+      <Header cart={cart} />
+
+      <div className="checkout-page">
+        <div className="page-title">Review your order</div>
+
+        <div className="checkout-grid">
+          <OrderSummary deliveryOptions={deliveryOptions} cart={cart} />
+
+          <div className="payment-summary">
+            <div className="payment-summary-title">Payment Summary</div>
+            <PaymentSummary paymentSummary={paymentSummary} />
+
+            <button className="place-order-button button-primary">
+              Place your order
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { formatMoney } from "../../utils/money";
+
+import "./OrdersPage.css";
+import { Header } from "../../components/Header";
+import { OrderProducts } from "./OrderProducts";
+// images served from public folder
+
+export function OrdersPage({ cart }) {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios.get("/api/orders?expand=products").then((response) => {
+      console.log(response.data);
+      setOrders(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <title>Orders</title>
+
+      <Header cart={cart} />
+
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+
+        <div className="orders-grid">
+          {orders.length > 0 &&
+            orders.map((order) => {
+              return (
+                <div key={order.id} className="order-container">
+                  <div className="order-header">
+                    <div className="order-header-left-section">
+                      <div className="order-date">
+                        <div className="order-header-label">Order Placed:</div>
+                        <div>
+                          {dayjs(order.orderTimesMs).format("dddd MMMM D")}
+                        </div>
+                      </div>
+                      <div className="order-total">
+                        <div className="order-header-label">Total:</div>
+                        <div>{formatMoney(order.totalCostCents)}</div>
+                      </div>
+                    </div>
+
+                    <div className="order-header-right-section">
+                      <div className="order-header-label">Order ID:</div>
+                      <div>{order.id}</div>
+                    </div>
+                  </div>
+                  <OrderProducts order={order} />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+}
